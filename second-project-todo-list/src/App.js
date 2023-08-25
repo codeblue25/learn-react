34054692b0c30d3189f82e9dedaa0380937c37b9
codeblue25 <chois1031@gmail.com>
2023-08-25 10:31:55 +0900

    Craete Todo list Array

--- second-project-todo-list/src/App.js
+++ second-project-todo-list/src/App.js
@@ -2,22 +2,23 @@ import { useState } from "react";
 
 function App() {
   const [todo, setTodo] = useState("");
+  const [todoArr, setTodoArr] = useState([]);
 
   const inputTodo = (e) => {
     setTodo(e.target.value);
   };
-
   const submitTodo = (e) => {
     e.preventDefault();
     if (todo === "") {
       return;
     }
     setTodo("");
-    console.log(todo);
+    setTodoArr((currentTodoArr) => [todo, ...currentTodoArr]);
   };
 
   return (
     <div>
+      <h1>My Todos ({todoArr.length})</h1>
       <form onSubmit={submitTodo}>
         <input
           type="text"
@@ -27,6 +28,12 @@ function App() {
         />
         <button>Add todo</button>
       </form>
+      <hr />
+      <ul>
+        {todoArr.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
