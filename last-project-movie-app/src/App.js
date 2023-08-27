11deeeea97428ca45da19828b7a9f54b6fbbca15
codeblue25 <chois1031@gmail.com>
2023-08-27 11:28:43 +0900

    Create movie cards

--- last-project-movie-app/src/App.js
+++ last-project-movie-app/src/App.js
@@ -1,29 +1,46 @@
 import { useEffect, useState } from "react";
+import styles from './App.module.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([])
 
-  useEffect(() => {
+  const getMoives = async() => {
+    const response = await 
     fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
-      .then((res) => res.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    const json = await response.json()
+    setMovies(json.data.movies);
+    setLoading(false);
+
+    console.log(json.data.movies)
+   }
+
+  useEffect(() => {
+    getMoives()
   }, []);
 
   return (
     <div>
       <h1>The Movies !</h1>
       {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {movies.map((movie, index) => (
-          <li key={index}>
-            {movie.title}
-          </li>
+
+
+        {movies.map((movie) => (
+          <div className={styles.card} key={movie.id}>
+            <div className={styles.image}>
+              <img src={movie.medium_cover_image} />
+            </div>
+            <div>
+              <h3>{movie.title}</h3>
+              {movie.genres.map((genere) => (
+                <div className={styles.chip} key={genere}>
+                  {genere}
+                </div>
+              ))}
+              <p>{movie.summary}</p>
+            </div>
+          </div>
         ))}
-      </ul>
     </div>
   );
 }
